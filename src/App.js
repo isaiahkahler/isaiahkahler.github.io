b86131e35a049468ecfa17bb69a2541eac8c9100
Isaiah Kahler <isaiahwkahler@gmail.com>
2018-06-23 18:36:53 -0400

    implement bulma

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bulma/css/bulma.min.css';
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        page
-      </div>
+      <div className="page">{this.props.children}</div>
+    );
+  }
+}
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav className="navbar" role="navigation">
+        <div className="navbar-brand">
+          <div className='navbar-item'>isaiah kahler</div>
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" onClick={() => {alert("oh");}}>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <div className="navbar-item">about</div>
+            <div className="navbar-item">skills</div>
+            <div className="navbar-item">projects</div>
+            <div className="navbar-item">contact</div>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
@@ -15,8 +39,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Page />
-        yeet
+        <Page> 
+          <Nav />
+        </Page>
+        <Page> </Page>
       </div>
     );
   }
