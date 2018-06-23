69b6f194a8167b35df8796bb055e39fd23369b95
Isaiah Kahler <isaiahwkahler@gmail.com>
2018-06-23 19:32:45 -0400

    images

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import 'bulma/css/bulma.min.css';
 class Page extends Component {
   render() {
     return (
-      <div className="page">{this.props.children}</div>
+      <div className="page" id={this.props.id}>
+        {this.props.children}
+      </div>
     );
   }
 }
@@ -13,24 +15,44 @@ class Page extends Component {
 class Nav extends Component {
   render() {
     return (
-      <nav className="navbar" role="navigation">
+      <nav className="navbar is-fixed-top" role="navigation">
         <div className="navbar-brand">
-          <div className='navbar-item'>isaiah kahler</div>
-          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" onClick={() => {alert("oh");}}>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
+          <div className="navbar-item">isaiah kahler</div>
+          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false"
+            onClick={() => {
+              document
+                .querySelector('.navbar-burger')
+                .classList.toggle('is-active');
+              document
+                .querySelector('.navbar-menu')
+                .classList.toggle('is-active');
+            }}
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
         </div>
         <div className="navbar-menu">
           <div className="navbar-end">
-            <div className="navbar-item">about</div>
-            <div className="navbar-item">skills</div>
-            <div className="navbar-item">projects</div>
-            <div className="navbar-item">contact</div>
+            <a className="navbar-item" href="#about">about</a>
+            <a className="navbar-item" href="#skills">skills</a>
+            <a className="navbar-item" href="#projects">projects</a>
+            <a className="navbar-item" href="#experience">experience</a>
+            <a className="navbar-item" href="#contact">contact</a>
           </div>
         </div>
       </nav>
+    );
+  }
+}
+
+class LevelTitle extends Component {
+  render() {
+    return (
+      <div className="level">
+        <div className="level-item"></div>
+      </div>
     );
   }
 }
@@ -39,10 +61,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Page> 
-          <Nav />
+        <Nav />
+        <Page id="about">
+          <section class="hero is-fullheight">
+            <div class="hero-body">
+              <div class="container">
+                <h1 class="title">Hey! I'm Isaiah.</h1>
+                <h2 class="subtitle">
+                  I'm a computer science student and I code.
+                </h2>
+              </div>
+            </div>
+          </section>
         </Page>
-        <Page> </Page>
+        <Page id="skills"> </Page>
+        <Page id="projects" />
+        <Page id="experience" />
+        <Page id="contact" />
       </div>
     );
   }
