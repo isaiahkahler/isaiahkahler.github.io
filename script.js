926e9248643d2b4c33fe042e9c51bef398750442
Isaiah Kahler <isaiahkahler@thicc-mac.local>
2018-11-07 01:16:10 -0500

    im going api

---
+++ script.js
@@ -0,0 +1,44 @@
+
+async function getData () {
+    let response = await fetch('https://api.keyvalue.xyz/aa6b42ba/isaiahkahler', {
+        method: "GET"
+    });
+    let json = await response.json();
+    return json;
+}
+
+let submitted = false;
+
+async function postData(data) {
+    if(!submitted){
+        fetch('https://api.keyvalue.xyz/aa6b42ba/isaiahkahler', {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+    }
+}
+
+let list = document.getElementById('namelist');
+function placeNames() {
+    getData().then(response => {
+        list.innerHTML = '';
+        let names = response.map(item => {
+            let li = document.createElement('li')
+            let p = document.createElement('p');
+            p.innerHTML = item
+            li.appendChild(p);
+            list.appendChild(li);
+        })
+    })
+}
+
+placeNames();
+
+let form = document.getElementById('form');
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    let names = await getData();
+    names.push(event.srcElement[0].value)
+    await postData(names);
+    setTimeout(() => placeNames(), 1000)
+})
